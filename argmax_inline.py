75c89cda716128d829c9e358813d8d207051acc4
Piotr Ceranek <petr.ceranek@gmail.com>
2016-01-27 23:52:19 +0100

    Program zgodny z wymaganiami

---
+++ argmax_inline.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import functools
+__author__ = 'piotrowy'
+
+
+def main():
+    a = [2, 3, 4, 5, 8, 7, 6, 4]
+    print(([a[x] if  for x in range(0, len(a))]))
+
+if __name__ == '__main__':
+    main()
--- sygnaly.py
+++ sygnaly.py
@@ -1,39 +1,29 @@
 import wave
-import sys
-import os
 import numpy
 import struct
 import functools
 
 
-def main():
-    hits = 0
-    count = 0
-    for file in os.listdir(os.getcwd() + '/train'):
-        signal = wave.open('./train/' + file)
+def main(argv):
+    signal = wave.open(argv)
+    max_fouriers = []
+    channels, samp_width, frame_rate, nframes, comp_type, comp_name = signal.getparams()
+    time_frame_size = int(frame_rate/5)
+    frames = signal.readframes(channels * nframes)
+    out = struct.unpack_from("%dh" % nframes * channels, frames)
+    mono = [(out[x]+out[x+1])/2 for x in range(0, len(out), 2)] if channels == 2 else [out[x] for x in range(len(out))]
+    avg_mono = functools.reduce(lambda x, y: x+y, mono)/len(mono)
+    mono = [mono[x] - avg_mono for x in range(len(mono))]
+    for i in range(0, len(mono), time_frame_size):
+        fourier = numpy.fft.fft(mono[i:i + time_frame_size])
+        if len(fourier) == time_frame_size:
+            max_fouriers.append(numpy.asarray([abs(fourier[i]) for i in range(1, 56, 1)]).argmax(axis=0)*frame_rate/time_frame_size)
+    max_fouriers.sort()
 
-        max_fouriers = []
+    if len(max_fouriers) > 0 and max_fouriers[int(len(max_fouriers)/2)] < 165:
+        print('M')
+    elif max_fouriers[int(len(max_fouriers)/2)] >= 165:
+        print('K')
 
-        channels, samp_width, frame_rate, nframes, comp_type, comp_name = signal.getparams()
-        time_frame_size = int(frame_rate/5)
-        frames = signal.readframes(channels * nframes)
-        out = struct.unpack_from("%dh" % nframes * channels, frames)
-        mono = [(out[x]+out[x+1])/2 for x in range(0, len(out), 2)] if channels == 2 else [out[x] for x in range(len(out))]
-        avg_mono = functools.reduce(lambda x, y: x+y, mono)/len(mono)
-        mono = [mono[x] - avg_mono for x in range(len(mono))]
-        for i in range(0, len(mono), time_frame_size):
-            fourier = numpy.fft.fft(mono[i:i + time_frame_size])
-            if len(fourier) == time_frame_size:
-                max_fouriers.append(numpy.asarray([abs(fourier[i]) for i in range(1, 56, 1)]).argmax(axis=0)*frame_rate/time_frame_size)
-        max_fouriers.sort()
-
-        if len(max_fouriers) > 0:
-            count += 1
-            if ((max_fouriers[int(len(max_fouriers)/2)]) < 165 and 'M' in file) or ((max_fouriers[int(len(max_fouriers)/2)]) >= 165 and 'K' in file):
-                hits += 1
-            print('freq: ' + str((max_fouriers[int(len(max_fouriers)/2)])) + ' hz ' + file + ' : ' + str((max_fouriers[int(len(max_fouriers)/2)])) + ' : ' + str(hits))
-        else:
-            print(file + ' : ' + str(frame_rate) + ' : ' + str(nframes))
-    print(str(hits/count))
 if __name__ == '__main__':
     main()
